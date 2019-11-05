8234adf5d937006a6bd866f1c32c4f943ffe0789
ShadowCharmer21 <56047126+ShadowCharmer21@users.noreply.github.com>
2019-11-05 12:14:27 -0800

    made the car have better controls

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     public float speed = 5.0f;
-    public float turnSpeed;
-    
+    public float turnSpeed = 50.0f;
+    public float horizontalInput;
+    public float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,10 @@ public class PlayerControler : MonoBehaviour
     void Update()
     {
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
