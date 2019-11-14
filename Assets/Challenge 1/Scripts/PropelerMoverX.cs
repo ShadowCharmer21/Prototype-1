38640ba61f56bca59179a09431e111c1ac0d435a
ShadowCharmer21 <56047126+ShadowCharmer21@users.noreply.github.com>
2019-11-14 11:58:04 -0800

    fdhewUFH

--- Assets/Challenge 1/Scripts/PropelerMoverX.cs
+++ Assets/Challenge 1/Scripts/PropelerMoverX.cs
@@ -2,20 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayerX : MonoBehaviour
+public class PropelerMoverX : MonoBehaviour
 {
-    public GameObject plane;
-    private Vector3 offset = new Vector3(30, 0, 10);
+    public float rotationSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = plane.transform.position + offset;
+        transform.Rotate(Vector3.down * rotationSpeed);
     }
 }
